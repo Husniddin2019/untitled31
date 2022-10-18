99f693f68f11fcea7c36a15d07a8d34969c735a7
Husniddin2019 <husni_91@mail.ru>
2022-10-18 11:15:58 +0500

    Initial commit

---
+++ lib/pages/update.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class update extends StatefulWidget {
+  const update({Key? key}) : super(key: key);
+
+  @override
+  State<update> createState() => _updateState();
+}
+
+class _updateState extends State<update> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
